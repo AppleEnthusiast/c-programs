1492e4a5705f0d7c375504cbac37239ecacdb847
Teoman Dogueri <Teoman.dogueri@icloud.com>
2025-08-17 16:24:10 +0200

    Add comment in German to explain why include-statement is needed

--- cteo0001.c
+++ cteo0001.c
@@ -1,3 +1,10 @@
+/*
+Die include-Anweisung dient dazu, Funktionen aus anderen Bibliotheken
+nutzbar zu machen. Über die Header-Datei stdio.h erreicht man eine
+Bibliothek mit Standardfunktionen für die Ein- und Ausgabe, darunter auch 
+die Funktion printf.
+*/
+
 #include <stdio.h>
 
 int main(void) {

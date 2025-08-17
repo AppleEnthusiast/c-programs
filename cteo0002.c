e6895f62f4d1026f68469c0a3d0349657e192e84
Teoman Dogueri <Teoman.dogueri@icloud.com>
2025-08-17 16:56:50 +0200

    Update cteo0002.c to read user input and calculate total price
    - Replace fixed values with scanf input for int and double
    - Prompt user for quantity and price
    - Print total cost by multiplying quantity and price

--- cteo0002.c
+++ cteo0002.c
@@ -5,11 +5,18 @@ int main(void){
  // Die Deklaration dient dazu, einer Variablen einen Namen zu geben
  // und sie einem Dateityp zuzuordnen
 
- int anzahl = 3;
- double preis = 5.2;
+ int anzahl;
+ double preis;
 
- printf("Anzahl: %d\n", anzahl);
+ printf("Anzahl eingeben: ");
+ scanf("%d", &anzahl);
+
+ printf("Preis eingeben: ");
+ scanf("%lf", &preis);
+ 
+ printf("\nAnzahl: %d\n", anzahl);
  printf("Preis: %.2f Euro\n", preis);
+ printf("Total: %.2f Euro\n", anzahl * preis);
 
  printf("\n");
  return 0;

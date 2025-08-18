e3dee795523c0907321b6738f57315abdbb372f4
Teoman Dogueri <Teoman.dogueri@icloud.com>
2025-08-18 20:02:15 +0200

    Create first_function_definition with a function that prints out a logo

---
+++ first_function_definition.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void myLogo(void);
+int main(void){
+
+ myLogo();
+ printf("\n");
+ return 0;
+}
+void myLogo(void){
+ printf("\n------------------------------------\n");
+ printf("-  Teoman Dogueri is the greatest  -\n");
+ printf("------------------------------------\n");
+}

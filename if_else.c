7603d137fe75594f0029c65fd58390bd07e13fd2
Teoman Dogueri <Teoman.dogueri@icloud.com>
2025-08-21 22:36:02 +0200

    Update if_else.c
    Change language from German to Englisch
    Remove variables and if-else statement in main
    Define function greeting that asks user for his name and age
    Add if-else statement in function greeting that evaluates username
    Display greeting if user is "Teo" and ask for his age

--- if_else.c
+++ if_else.c
@@ -1,17 +1,35 @@
 #include <stdio.h>
+#include <string.h>
+
+void greeting(void);
 
 int main(void){
 
- int age = 0;
-
- printf("Wie alt bist du? ");
- scanf("%d", &age);
-
- if(age >= 18)
-	printf("Du bist erwachsen.");
- else
-	printf("Du bist noch nicht erwachsen.\nDas kommt noch. Früher oder später.");
-
+ greeting();
  printf("\n");
  return 0;
 }
+void greeting(void){
+	char name[100];
+	int age = 0;
+	
+	printf("What's your name? ");
+	scanf("%s",name);
+	
+	if(strcmp(name,"Teo") == 0){
+		printf("This is a beautiful name.\n");
+
+		printf("How old are you, %s? ",name);	
+		scanf("%d",&age);
+
+		printf("This is a beautiful age.\n");
+
+		printf("Your name is %s and you are %d years old.\n",name, age);
+		printf("It's great you are here.\n");
+	}
+	else{
+		printf("I don't know you.");
+	}
+
+
+}//end of function greeting

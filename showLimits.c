8cbb24e3846afb702219a6f098dc7cc7b0eb626e
Teoman Dogueri <Teoman.dogueri@icloud.com>
2025-08-24 16:57:54 +0200

    Add function showMenuwhich asks user to choose data type for displaying limits

--- showLimits.c
+++ showLimits.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 #include <limits.h>
 
-int main(void){
+void showMenu();
 
+int main(void){
+ showMenu();
  printf("\n");
  return 0;
 }
-
+void showMenu(){
+	printf("This program shows you the limit of numbers.\n");
+	printf("The limits of which data type do you want to see?\n");
+	printf("-------------------------------------------------\n");	
+	printf("(a) short\n");	
+	printf("(b) int\n");	
+	printf("(c) long\n");	
+	printf("-------------------------------------------------\n");	
+}

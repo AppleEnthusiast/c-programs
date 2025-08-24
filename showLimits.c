d235c31769720baf12cc4c93f18268fa0479a47d
Teoman Dogueri <Teoman.dogueri@icloud.com>
2025-08-24 17:05:58 +0200

    Add if-else statements with user choice and improved messages
    Introduce a simple menu where the user can select between short, int, or long.
    Implement if-else branching to handle the different options.
    Improve the output text with clearer and more natural English expressions.

--- showLimits.c
+++ showLimits.c
@@ -3,17 +3,32 @@
 
 void showMenu();
 
-int main(void){
- showMenu();
- printf("\n");
- return 0;
+int main(void) {
+    showMenu();
+    printf("\n");
+    return 0;
 }
-void showMenu(){
-	printf("This program shows you the limit of numbers.\n");
-	printf("The limits of which data type do you want to see?\n");
-	printf("-------------------------------------------------\n");	
-	printf("(a) short\n");	
-	printf("(b) int\n");	
-	printf("(c) long\n");	
-	printf("-------------------------------------------------\n");	
+
+void showMenu() {
+    printf("This program shows the limits of different number types.\n");
+    printf("Which data type limits would you like to see?\n");
+    printf("-------------------------------------------------\n");	
+    printf("(a) short\n");	
+    printf("(b) int\n");	
+    printf("(c) long\n");	
+    printf("-------------------------------------------------\n");	
+
+    char choice = '\0';
+    scanf("%c", &choice);
+
+    if (choice == 'a') {
+        printf("You selected: short");
+    } else if (choice == 'b') {
+        printf("You selected: int");
+    } else if (choice == 'c') {
+        printf("You selected: long");
+    } else {
+        printf("Invalid option");
+    }
 }
+

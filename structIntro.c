79bc39807ed7f5309c6e0089badd2721021b6340
AppleEnthusiast <Teoman.dogueri@icloud.com>
2025-08-26 20:46:27 +0200

    Add comma after struct definition to fix mistake; Remove escape sequence \n from user input; Display student's name that was user input

--- structIntro.c
+++ structIntro.c
@@ -5,12 +5,14 @@ struct Student {
  char name[50];
  int age;
  float grade;
-}
+};
 int main(void){
  struct Student student1;
 
  printf("Enter student's name: ");
- fgets(student1.name, 50, stdin);
+ fgets(student1.name, sizeof(student1.name), stdin);
+ student1.name[strcspn(student1.name,"\n")] = '\0';
+ printf("Student 1: %s",student1.name);
 
  printf("\n");
  return 0;
